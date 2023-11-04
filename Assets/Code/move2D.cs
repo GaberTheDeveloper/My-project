a5c7fd9ce6a7294177588ad6461263a637d284d0
Bennett Smrdel <fireknights318@gmail.com>
2023-11-03 21:40:01 -0500

    fix: Player disapearing when moving

--- Assets/Code/move2D.cs
+++ Assets/Code/move2D.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class move2D : MonoBehaviour
 {
-    public Rigidbody2D MyRigidBody2D;
+    [SerializeField] private Rigidbody2D MyRigidBody2D;
     [SerializeField] private int playerId;
     [SerializeField] private float speed = 5f;
 
@@ -16,21 +16,23 @@ public class move2D : MonoBehaviour
 
     private void HandleMovement()
     {
-        if (Input.GetKey(KeyCode.A))
+        float x = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey(KeyCode.A) && playerId == 1)
         {
-            float x = Input.GetAxis("Horizontal");
-            Vector3 movement = new Vector3(x, transform.position.y, transform.position.z);
-            transform.Translate(movement * speed * Time.deltaTime);
-        } else if (Input.GetKey(KeyCode.D))
+            Vector3 movement = new Vector3(x * speed, 0, 0);
+            movement *= Time.deltaTime;
+            transform.Translate(movement);
+        } else if (Input.GetKey(KeyCode.D) && playerId == 1)
         {
-            float x = Input.GetAxis("Horizontal");
-            Vector3 movement = new Vector3(x * speed * Time.deltaTime, transform.position.y, transform.position.z);
+            Vector3 movement = new Vector3(x * speed, 0, 0);
+            movement *= Time.deltaTime;
             transform.Translate(movement);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) == true)
         {
             MyRigidBody2D.velocity = Vector2.up * 10;
-        }ign
+        }
     }
 }
