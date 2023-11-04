85b5a07b073294d63d2a455f2800eaae56d403e4
Bennett Smrdel <fireknights318@gmail.com>
2023-11-03 22:14:52 -0500

    feat: Player flips to movement direction

--- Assets/Code/move2D.cs
+++ Assets/Code/move2D.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class move2D : MonoBehaviour
@@ -7,6 +9,7 @@ public class move2D : MonoBehaviour
     [SerializeField] private Rigidbody2D MyRigidBody2D;
     [SerializeField] private int playerId;
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
 
 
     void Update()
@@ -16,23 +19,41 @@ public class move2D : MonoBehaviour
 
     private void HandleMovement()
     {
-        float x = Input.GetAxis("Horizontal");
+        // x will be set equal to -1 or 1
+        // -1 if moving left
+        // 1 if moving right
+        float x = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKey(KeyCode.A) && playerId == 1)
+        // Player 1 movement
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && playerId == 1)
+        {
+            Vector3 movement = new Vector3(x * speed, 0, 0); // Only changes the x value, the y and z values are adding 0
+            movement *= Time.deltaTime;
+            transform.Translate(movement);
+            FlipPlayer(x);
+        }
+
+        // Player 2 movement
+        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) && playerId == 2)
         {
             Vector3 movement = new Vector3(x * speed, 0, 0);
             movement *= Time.deltaTime;
             transform.Translate(movement);
-        } else if (Input.GetKey(KeyCode.D) && playerId == 1)
-        {
-            Vector3 movement = new Vector3(x * speed, 0, 0);
-            movement *= Time.deltaTime;
-            transform.Translate(movement);
+            FlipPlayer(x);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) == true)
         {
-            MyRigidBody2D.velocity = Vector2.up * 10;
+            MyRigidBody2D.velocity = Vector2.up * jumpForce;
         }
+    }
+
+    // Make player face movement direction
+    private void FlipPlayer(float x)
+    {
+        if (x > 0 && transform.localScale.x > 0)
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        else if (x < 0 && transform.localScale.x < 0)
+            transform.localScale = new Vector2(Math.Abs(transform.localScale.x), transform.localScale.y);
     }
 }
